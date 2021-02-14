96541797d62505c4592649a01fe45893a55a5a0f
ekjob <bojke101@hotmail.com>
2021-02-14 19:20:52 +0000

    added git setup script

---
+++ setupgit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# setup git user email and password
+echo "setup git user email and password"
+git config --global user.email "bojke101@hotmail.com"
+git config --global user.name "ekjob"
